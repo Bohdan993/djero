4e81027bca604be70b4b617e81d86a8217f2b90f
bohdan993 <bohdan1trush@gmail.com>
2021-04-12 12:42:28 +0300

    bm

--- app/js/model/App/BurgerMenu.js
+++ app/js/model/App/BurgerMenu.js
@@ -3,6 +3,8 @@ import { throttle } from './Helpers'
 const BurgerMenu = (menuBtn, items, parent, body) => {
 	let menuOpen = false
 
+	console.log(manuOpen)
+
 	function createMenu(items){
 		let menuItems = Array.from(items)
 		let menu = `
